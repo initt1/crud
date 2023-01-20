8c1b06e7f447d249753d8e7753324122e442e821
initt1 <juliamaia301@gmail.com>
2023-01-20 13:12:07 -0300

    up

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ $checkbox.addEventListener('change', function(){
     $html.classList.toggle('clown-mode')
 })
 
-function toggleTheme() {
-    var body = document.getElementsByTagName("body")[0];
-    if (body.classList.contains("clown-mode")) {
-    body.classList.remove("clown-mode");
-    } else {
-    body.classList.add("clown-mode");
-    }
-}
+// function toggleTheme() {
+//     var body = document.getElementsByTagName("body")[0];
+//     if (body.classList.contains("clown-mode")) {
+//     body.classList.remove("clown-mode");
+//     } else {
+//     body.classList.add("clown-mode");
+//     }
+// }
